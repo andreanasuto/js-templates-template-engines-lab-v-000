7e7364792acc85b9fe672cdbf5df161f6b64396b
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-03-20 20:04:00 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,25 @@
+function postComment() {
+  var comment = document.getElementById("commentContent").value
+  var author = document.getElementById("commentAuthor").value
+
+  var commentTemplate = document.getElementById("comment-template").innerHTML
+  var templateFn = _.template(commentTemplate)
+  var commentsDiv = document.getElementById("comment")
+  var templateHTML = templateFn({ 'comment': comment, 'author': author})
+  commentsElements.innerHTML += templateHTML
+}
+
+function createPost() {
+
+  var title = document.getElementById("postTitle").value
+  var author = document.getElementById("postAuthor").value
+  var content = document.getElementById("postBody").value
+
+  var postTemplate = document.getElementById("post-template").innerHTML
+  var pagetemplateFn = _.template(pageTemplate)
+  var templateFn = _.template(postTemplate)
+  var postElement = document.getElementById("post")
+  var templateHTML = templateFn({ 'title': title, 'author': author, 'content': content})
+  postElement.innerHTML += templateHTML
+
+}
