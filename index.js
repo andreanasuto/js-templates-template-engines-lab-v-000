6816bd20b16ee1212e5e62ac123d99a15dceb2c4
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-03-20 20:36:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,8 +16,17 @@ function createPost() {
   var content = document.getElementById("postBody").value
 
   var postTemplate = document.getElementById("post-template").innerHTML
-  var templateFn = _.template(postTemplate)
+  var postTemplateFn = _.template(postTemplate)
   var postElement = document.getElementById("post")
+  var templateHTML = postTemplateFn({ 'title': title, 'author': author, 'content': content})
+
+  // you gotta insert the post template in the page template
+  // then you gotta insert the page template in the main div
+
+  var pageTemplate = document.getElementById("page-template").innerHTML
+  var pageTemplateFn = _.template(pageTemplate)
+  var mainDiv = document.getElementsByTag('main')
+
   var templateHTML = templateFn({ 'title': title, 'author': author, 'content': content})
   postElement.innerHTML += templateHTML
 
